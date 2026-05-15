62a00ab3e42b1f380ef725e6f3425b1bcf231b5a
iam6ukk <iann6ukk@gmail.com>
2026-05-15 16:13:54 +0900

    chore: initialize project

---
+++ backend/src/main/java/com/example/miniorder/MiniOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniOrderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/miniorder/MiniOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
